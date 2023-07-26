3e099b2159ee4b9a7055b1e73dae5af80c362b4c
Natalya Demidenko <natalucia777@ukr.net>
2023-07-26 22:28:13 +0300

    hw-react-4-3-im-6

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,23 +2,15 @@
 import { useState } from 'react';
 import { ModalImg } from '../Modal/Modal';
 import { ImgItem, ImgItemImg } from './ImageGalleryItem.styled';
+
 function ImageGalleryItem({ item }) {
-  // state = {
-  //   isModalOpen: false,
-  // };
 const [isModalOpen, setIsModalOpen] = useState(false);
 const openModal = () => {
-  // this.setState({ isModalOpen: true });
     setIsModalOpen(true);
   };
-
 const closeModal = () => {
-  // this.setState({ isModalOpen: false });
   setIsModalOpen(false);
   };
-  // render() {
-    // const { item } = this.props;
-    // const { isModalOpen } = this.state;
     return (
       <div>
         <ImgItem className="gallery-item" key={item.id} >
